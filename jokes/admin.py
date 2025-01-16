5e96ab75d116e69cc80d80f3b0d8c0cc2f717a17
LoueiVeiga <misakiyuuko15@GMAIL.COM>
2025-01-16 11:07:14 +0800

    urls and slugs

--- jokes/admin.py
+++ jokes/admin.py
@@ -9,7 +9,7 @@ class JokeAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('created', 'updated')
+            return ('slug','created', 'updated')
         
         return ()
 # Register your models here.
