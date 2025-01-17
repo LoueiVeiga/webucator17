2345a7f6c43901ad3e4a44215c9c964dccde4f02
LoueiVeiga <misakiyuuko15@GMAIL.COM>
2025-01-17 13:47:46 +0800

    joke categories

--- common/utils/email.py
+++ common/utils/email.py
@@ -4,7 +4,7 @@ from sendgrid.helpers.mail import Mail
 from django.conf import settings
 
 
-def send_email(to, subject, content, sender='kayliegalado17@gmail.com'):
+def send_email(to, subject, content, sender='michael_galado@emb.gov.ph'):
     sg = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY)
     mail = Mail(
         from_email=sender,
